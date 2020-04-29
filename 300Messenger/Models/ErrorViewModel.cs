fb611cc2364405b00ee4b4056f208ff7310fd43a
Christian Schmid <42004356+cschmid92@users.noreply.github.com>
2020-04-28 20:59:23 -0700

    Add project files.

---
+++ 300Messenger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _300Messenger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
